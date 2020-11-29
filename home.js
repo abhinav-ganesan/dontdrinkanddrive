31e4e29392fc6027744828f26f796b8b455855f7
abhinav-ganesan <69860201+abhinav-ganesan@users.noreply.github.com>
2020-11-29 11:39:10 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,32 @@
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+var donateButton = document.getElementById('donate');
+var donateLink = "https://www.madd.org/donate-now/?utm_expid=.bKPQwiK9RLGomb7kUTM9hQ.0&utm_referrer="
+
+donateButton.onclick = () => {
+  window.open(donateLink);
+}
+
+var exploreButton = document.getElementById('explore');
+var exploreLink = "https://usa.streetsblog.org/2020/11/16/feds-to-finally-explore-drunk-driving-prevention-tech/"
+
+exploreButton.onclick = () => {
+  window.open(exploreLink);
+}
